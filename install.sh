22808750f4388525b2826596cbedd8e10a152a95
MoBoo <moboo@online.de>
2021-09-30 18:43:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ pip3 install docker-compose
 usermod -aG docker $SUDO_USER
 
 # Install VSCodium
-snap install --classic codium
+snap install --classic code
 
 # Install Sublime Text Editor
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
